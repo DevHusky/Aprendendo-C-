3e30d0729e58aedec2f4b332d91be6b25b07c94d
Naum <naumhenriquer@gmail.com>
2024-08-29 01:05:18 -0300

    Colocando as questoes no codigo

--- Area de um Circulo/Program.cs
+++ Area de um Circulo/Program.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿/*Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+casas decimais conforme exemplos.
+Fórmula:  = .
+Considere o valor de π = 3.14159
+
+Exemplos:
+Entrada: Saída:
+2.00 A=12.5664
+Entrada: Saída:
+100.64 A=31819.3103
+Entrada: Saída:
+150.00 A=70685.7750
+*/
+using System;
 
 class Area
 {
